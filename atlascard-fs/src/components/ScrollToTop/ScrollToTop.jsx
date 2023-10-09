719f7ee37baac5a1d73f3c067d767078a1d451e1
Fjolla2002 <fjolla.selimi17@gmail.com>
2023-10-09 14:53:04 +0200

    Made some changes to scroll to top component, and replaced react logo with atlascard logo

--- atlascard-fs/src/components/ScrollToTop/ScrollToTop.jsx
+++ atlascard-fs/src/components/ScrollToTop/ScrollToTop.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './scrollToTop.scss';
-import arrowUp from '../../assets/images/arrowUp.svg'
+import arrowUp from '../../assets/images/arrowUp.svg';
+import arrowDown from '../../assets/images/arrowDown.svg';
 
 const ScrollToTop = () => {
+    const [showArrowUp, setShowArrowUp] = useState(false);
+
     const handleScroll = () => {
-        window.scrollTo({
-            top: 0, left: 0, behavior: 'smooth'
-        })
-    }
+        setShowArrowUp(window.scrollY > window.screen.height);
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    };
+
+    const scrollToBottom = () => {
+        window.scrollTo({ top: document.body.scrollHeight, left: 0, behavior: 'smooth' });
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
         <div className='scrollTop'>
-            <button onClick={handleScroll}>
-                <img src={arrowUp} alt="arrowup" />
+            <button onClick={showArrowUp ? scrollToTop : scrollToBottom}>
+                <img src={showArrowUp ? arrowUp : arrowDown} alt={showArrowUp ? 'arrow up' : 'arrow down'} />
             </button>
         </div>
-    )
-}
+    );
+};
 
 export default ScrollToTop;
